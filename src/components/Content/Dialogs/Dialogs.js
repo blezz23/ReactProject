adc21408d39b5a5a522252a6b28ea943cd9ee53b
NIKITA <delya99@mail.ru>
2020-07-11 14:50:53 +0300

    Lesson 24

--- src/components/Content/Dialogs/Dialogs.js
+++ src/components/Content/Dialogs/Dialogs.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import s from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (props) => {
+    let path = "/dialogs/" + props.id;
+
+    return (
+        <div className={s.dialog}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+};
+
+const Message = (props) => {
+    return (
+        <div className={s.message}>{props.message}</div>
+    )
+};
+
+const dialogsData = [
+    {id: 1, name: 'Nikita'},
+    {id: 2, name: 'Maxim'},
+    {id: 3, name: 'Kostya'}
+];
+
+const messagesData = [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'Lol'},
+    {id: 3, message: 'Yo'}
+];
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
-                <div className={s.dialog + ' ' + s.active}>
-                    Nikita
-                </div>
-                <div className={s.dialog}>
-                    Maxim
-                </div>
-                <div className={s.dialog}>
-                    Konstantin
-                </div>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
             </div>
             <div className={s.messages}>
-                <div className={s.message}>Hi</div>
-                <div className={s.message}>Lol</div>
-                <div className={s.message}>Gif</div>
+                <Message message={messagesData[0].message} />
+                <Message message={messagesData[1].message} />
+                <Message message={messagesData[2].message} />
             </div>
         </div>
     );
--- src/components/Content/Main/Posts/Posts.js
+++ src/components/Content/Main/Posts/Posts.js
@@ -2,10 +2,17 @@ import React from 'react';
 import s from "./Posts.module.css"
 import Post from "./Post/Post";
 
+const postsData = [
+    {id: 1, message: 'lol'},
+    {id: 2, message: 'ne lol'},
+    {id: 3, message: 'da ne'}
+];
+
 const Posts = (props) => {
     return (<div className={s.posts}>
-            <Post message="lol"/>
-            <Post message="ne lol"/>
+            <Post message={postsData[0].message} />
+            <Post message={postsData[1].message} />
+            <Post message={postsData[2].message} />
         </div>
     );
 };
