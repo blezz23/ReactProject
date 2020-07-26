f143dc3a3cb16541d6206a81cda80df48bddcd82
NIKITA <delya99@mail.ru>
2020-07-26 18:16:26 +0300

    Lesson 54

--- src/components/Content/Friends/Friends.js
+++ src/components/Content/Friends/Friends.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import s from "./Friends.module.css"
+import * as axios from "axios";
+import userPhoto from "../../../Redux/123.png"
 
-const Friends = (props) => {
-    if (props.friendsData.length === 0) {
-        props.setFriends([
-            {
-                id: 1, name: "Nikita", followed: true,
-                avatar: "https://i.pinimg.com/originals/0c/a9/e2/0ca9e28dcb12dc698cfd2beda6d6fa64.jpg"
-            },
-            {
-                id: 2, name: "Maxim", followed: true,
-                avatar: "https://pm1.narvii.com/7388/55ffd10f441052afd8858b4a53216ae0a64883e3r1-2048-2048v2_hq.jpg"
-            },
-            {
-                id: 3, name: "Kostya", followed: true,
-                avatar: "https://www.meme-arsenal.com/memes/549e8c6d71ae27a2ebd13a7580d71d80.jpg"
-            }
-        ])
+class Friends extends React.Component {
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setFriends(response.data.items)
+            });
     }
 
-    return <div className={s.friends}>
-        {
-            props.friendsData.map(f => <div key={f.id}>
+    render() {
+        return <div className={s.friends}>
+            {
+                this.props.friendsData.map(f => <div key={f.id}>
                 <span>
                     <div>
-                        <img alt="" src={f.avatar}/>
+                        <img alt="" src={f.photos.small != null ? f.photos.small : userPhoto}/>
                     </div>
                     <div>
                         {f.name}
@@ -32,16 +25,17 @@ const Friends = (props) => {
                     <div>
                         {f.followed
                             ? <button onClick={() => {
-                                props.unfollow(f.id)
+                                this.props.unfollow(f.id)
                             }}>Удалить из друзей</button>
                             : <button onClick={() => {
-                                props.follow(f.id)
+                                this.props.follow(f.id)
                             }}>Добавить в друзья</button>}
                     </div>
                 </span>
-            </div>)
-        }
-    </div>
-};
+                </div>)
+            }
+        </div>
+    }
+}
 
 export default Friends;
